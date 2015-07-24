9be00ce7c5fc0f9f4fe1eb6377d2fb13d3d18c17
VidhyaKKrish <VidhyaKKrish@users.noreply.github.com>
2015-07-24 20:22:24 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,21 @@
+makeCacheMatrix           <- function(x = matrix()) {
+ 
+       m <- NULL
+                                
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+                                
+        get <- function() x
+                                
+        setinv <- function(inv) m <<- inv
+                                
+        getinv <- function() m
+                                
+        list(set = set, get = get,setinv = setinv,getinv = getinv)
+}
+ 
 cacheSolve <- function(x, ...) {
  
         m <- x$getinv()
